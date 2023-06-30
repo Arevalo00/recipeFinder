9adbcf81c95fd10f9421f0aa590f6a1704a2f069
Jacob Pudwill <jacobpudwill@gmail.com>
2023-06-29 19:47:17 -0500

    When find button is pressed on index.html page, the user is now redirected to the results.html page.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
 var search = document.getElementById("search-input");
 var edamamRequestUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=f18a3a55&app_key=8bb356bbf9943a27d2c2f82ce7546805";
 var rewind = "return";
-var input = document.getElementById("saveServer");
+// var input = document.getElementById("saveServer");
 
-localStorage.setItem("server", input.val());
+// localStorage.setItem("server", input.val());
 
 const url = 'https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf';
 const options = {
@@ -21,7 +20,7 @@ fetch(edamamRequestUrl)
    return response.json();
 })
 .then(function (data){
-   console.log(data);
+   // console.log(data);
 });
 
 // fetch request for words api
@@ -30,13 +29,11 @@ fetch(url, options)
    return response.text();
 })
 .then(function (data){
-   console.log(data);
+   // console.log(data);
+});
 
-})
-
-//adds return to index.html for find
-find.addEventListner('click', gohome);
-
-function gohome(){
-
-}
+// Takes user to results page
+$('#find').on('click', function(){
+   console.log('success');
+   document.location.replace('./results.html');
+});
